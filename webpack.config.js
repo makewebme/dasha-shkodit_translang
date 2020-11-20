fe809ff5e37e099833578eb58f6b5dc27926794c
Your Name <xen@xen.local>
2020-11-20 21:44:02 +0300

    Сверстаны и поправлены перве две секции

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,10 @@ module.exports = {
     new HtmlWebpackPugPlugin(),
 
     new MiniCssExtractPlugin({ filename: 'style.css' }), // Generating CSS
-    new CopyWebpackPlugin([{ from: 'img', to: 'img' }]) // Copy images
+    new CopyWebpackPlugin([
+      { from: 'img', to: 'img' }, // Copy images
+      { from: 'fonts', to: 'fonts' }, // Copy fonts
+    ])
   ],
 
   optimization: {
